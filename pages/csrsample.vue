2ff0a8a631f24cce6ad86179691a68f1ed9f4980
coolguyhong <podolsmith@naver.com>
2019-12-06 17:51:42 +0900

    csr ssr test

---
+++ pages/csrsample.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>
+      CSR - Sample Page
+    </h1>
+    <div class='div-contents'>
+      <ul v-for="item in items">
+        <li>NO : {{ item.id }}</li>
+        <li>제목 : {{ item.title }}</li>
+        <li>내용 : {{ item.body }}</li>
+      </ul>
+    </div>
+    <div class='div-btn'>
+      <nuxt-link to='/' class='button--grey'>index</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    name: "csrsample",
+    data() {
+      return {
+        items: []
+      }
+    },
+    methods: {
+      getBoardList() {
+        axios.get('https://jsonplaceholder.typicode.com/posts?id=1')
+        .then((res) => {
+          this.items = res.data
+        })
+      }
+    },
+    mounted() {
+      this.getBoardList()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
