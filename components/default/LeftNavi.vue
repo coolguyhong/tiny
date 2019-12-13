aa0cc3eee1bba84b0c54716b60df0a5d228aa69a
coolguyhong <podolsmith@naver.com>
2019-12-13 20:55:33 +0900

    component left navigation

---
+++ components/default/LeftNavi.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      v-model="drawerChild"
+      :mini-variant="miniVariantChild"
+      :clipped="clippedChild"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "LeftNavi",
+    props: ['drawer', 'miniVariant', 'clipped'],
+    data () {
+      return {
+        drawerChild: null,
+        miniVariantChild: null,
+        clippedChild: null,
+        items: [
+          {
+            icon: 'mdi-apps',
+            title: 'Welcome',
+            to: '/'
+          },
+          {
+            icon: 'mdi-chart-bubble',
+            title: 'Inspire',
+            to: '/inspire'
+          }
+        ]
+      }
+    },
+    watch: {
+      drawer (value) {
+        this.drawerChild = value
+      },
+      miniVariant (value) {
+        this.miniVariantChild = value
+      },
+      clipped (value) {
+        this.clippedChild = value
+      },
+      drawerChild (value) {
+        this.$emit('isDrawer', value)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,6 @@
 <template>
   <v-app light>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <LeftNavi @isDrawer="isDrawer" :drawer="drawer" :miniVariant="miniVariant" :clipped="clipped"></LeftNavi>
     <v-app-bar
       :clipped-left="clipped"
       fixed
@@ -89,28 +66,26 @@
 </template>
 
 <script>
+import LeftNavi from '@/components/default/LeftNavi.vue'
+
 export default {
+  components: {
+    LeftNavi
+  },
   data () {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
       title: 'Vuetify.js'
+    }
+  },
+  methods: {
+    isDrawer (value) {
+      this.drawer = value;
     }
   }
 }
